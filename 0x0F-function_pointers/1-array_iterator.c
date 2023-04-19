2a2fc4db110085e0d188d7a95c344aac2efd8609
Emakiflom <amankflom23@gmail.com>
2023-04-19 13:29:00 -0700

    2-int_index.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -7,14 +7,13 @@
  * @action: pointer to function
  * Return: void
  */
-
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	int i;
+
 	
 	if (array == NULL || action == NULL)
 		return;
-	
 	for (i = 0; i < (int) size; i++)
 	{
 		action(array[i]);
