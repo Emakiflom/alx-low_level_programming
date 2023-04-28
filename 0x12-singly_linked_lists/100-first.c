fd7c9d2c30595f4e31767b8e55aa87ed5d010754
Emakiflom <amankflom23@gmail.com>
2023-04-28 14:47:50 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * constructorRun - runs before main function is executed.
+ */
+
+void constructorRun(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+		"I bore my house upon my back!\n");
+}
