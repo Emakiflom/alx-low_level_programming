5cff7b6a1276684fdfb44d34d8d7949428113fb5
Emakiflom <amankflom23@gmail.com>
2023-07-10 12:00:17 -0700

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+  * create_file - creates a file with rw------- permissions
+  * @filename: name of the file, if NULL, return -1
+  * @text_content: contents of the file. If NULL, create an empty file
+  * Return: 1 on success, -1 on failure
+  */
+
+int create_file(const char *filename, char *text_content)
+{
+	int new_file, len, wr_stat;
+
+	if (filename == NULL)
+		return (-1);
+	new_file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (new_file == -1)
+		return (-1);
+	if (text_content == NULL)
+	{
+		close(new_file);
+		return (1);
+	}
+	for (len = 0; text_content[len]; len++)
+		;
+	wr_stat = write(new_file, text_content, len);
+	if (close(new_file) == -1)
+		return (-1);
+	return (wr_stat == -1 ? -1 : 1);
+}
