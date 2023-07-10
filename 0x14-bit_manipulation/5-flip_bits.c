92278d522179484365e1ac6bc2771cfc61e722e6
Emakiflom <amankflom23@gmail.com>
2023-07-10 12:42:01 -0700

    5-flip_bits.c

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * flip_bits - calculates the number of bits needed
+  * to get from one number to another
+  * @n: base number
+  * @m: number to transform to
+  * Return: the number of bit transformations needed
+  */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int i, res;
+
+	res = 0;
+	for (i = 8 * sizeof(n) - 1; i >= 0; i--)
+		if (((n ^ m) >> i) & 1)
+			res++;
+	return (res);
+}
