de64380535800a118d4ad5dfbcff94c01b84584f
Emakiflom <amankflom23@gmail.com>
2023-07-11 14:18:31 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,34 +1,46 @@
-#include "main.h"
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
 /**
- * read_textfile - reads a text file and prints it to the standard output
- * @filename: name of the file to be read
- * @letters: number of letters to read and print
- * Return: the number of letters printed, or 0 if it failed
+ * read_textfile - prints text from a file
+ *
+ * @filename: name of the file
+ * @letters: number of characters to read
+ *
+ * Return: actual number of letters read, 0 if end of file
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t fd, r, w;
-	char *buffer;
+	int file;
+	int length, wrotechars;
+	char *buf;
 
-	if (!filename)
+	if (filename == NULL || letters == 0)
+		return (0);
+	buf = malloc(sizeof(char) * (letters));
+	if (buf == NULL)
 		return (0);
 
-	buffer = malloc(letters * sizeof(char));
-	if (!buffer)
-		return (0);
-
-	fd = open(filename, O_RDONLY);
-	r = read(fd, buffer, letters);
-	w = write(STDOUT_FILENO, buffer, r);
-
-	if (fd == -1 || r == -1 || w == -1 || w != r)
+	file = open(filename, O_RDONLY);
+	if (file == -1)
 	{
-		free(buffer);
+		free(buf);
+		return (0);
+	}
+	length = read(file, buf, letters);
+	if (length == -1)
+	{
+		free(buf);
+		close(file);
 		return (0);
 	}
 
-	free(buffer);
-	close(fd);
+	wrotechars = write(STDOUT_FILENO, buf, length);
 
-	return (w);
+	free(buf);
+	close(file);
+	if (wrotechars != length)
+		return (0);
+	return (length);
 }
